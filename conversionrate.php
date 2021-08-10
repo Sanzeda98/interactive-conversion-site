53afa3cbabdbb1c002384030fdb2abc16865d922
Sanzeda98 <84757547+Sanzeda98@users.noreply.github.com>
2021-08-10 20:57:42 +0600

    Add files via upload

---
+++ conversionrate.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<div style="width: 450px;
+  height:  630px;
+  border: 5px solid black;
+  margin-left: 400px;">
+
+  <h3 style="color: red;">Page 2 [Conversion Rate]</h3>
+  <h3 style="color: red;">Conversion Site</h3>
+<p><a href="home.php">1.Home &nbsp</a><a href="conversionrate.php">2.Conversion Rate &nbsp</a><a href="history.txt">3.History</a></p>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 50px;display: inline-block; "> Feet to Inch</div>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1</div>
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 12</div>
+
+
+
+
+<div style=" font-size: 12px ; width:  80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 50px;display: inline-block; ">Kilometer to Meter</div>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1</div>
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1000</div>
+
+
+
+
+<div style=" font-size: 12px ; width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 50px;display: inline-block; ">Hour to Second</div>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1</div>
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 3600</div>
+
+
+
+
+<div style=" font-size: 12px ;width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 50px;display: inline-block; ">Feet to Centimeter</div>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1</div>
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 30.48</div>
+
+
+<div style=" font-size: 12px ;width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 50px;display: inline-block; ">USD to BDT</div>
+
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 1</div>
+<div style="width: 80px;height: 30px;border: 3px solid black;text-align: center;margin-top:30px;margin-left: 20px;display: inline-block;"> 85.88</div>
+
+
+
+
+
+</div>
+
+
+
+</body>
+</html>
